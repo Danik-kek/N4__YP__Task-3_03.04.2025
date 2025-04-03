3ea9ad9dfffd640946cef8b67cda5ac31216a068
Loli Admin <Loli Admin@DESKTOP-H4SR9KC>
2025-04-03 21:56:15 +0300

    Добавьте файлы проекта.

---
+++ N4__YP__Task-3_03.04.2025/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace N4__YP__Task_3_03._04._2025
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int A1 = 1;
+            int A2 = 20;
+            int A3 = 2;
+            double C = 100;
+            for (int X = A1; X <= A2; X += A3)
+            {
+                double X_cubed = Math.Pow(X, 3);
+                double Y;
+                if (X_cubed <= C)
+                {
+                    Y = X_cubed - C;
+                }
+                else
+                {
+                    Y = Math.Cbrt(X_cubed + C); // Кубический корень
+                }
+                Console.WriteLine($"X = {X}, Y = {Y}");
+            }
+        }
+    }
+}
